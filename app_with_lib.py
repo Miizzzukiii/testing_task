63e939f75c26ffbdbb3d166aad00a846eb89e287
Miizzzukiii <139797055+Miizzzukiii@users.noreply.github.com>
2024-11-12 17:12:19 +0500

    Create app_with_lib.py

---
+++ app_with_lib.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
+
+app = Flask(__name__)
+
+# Экспорт метрик для Prometheus
+metrics = PrometheusMetrics(app)
+
+@app.route('/')
+def hello():
+    return 'Привет, это тест!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
